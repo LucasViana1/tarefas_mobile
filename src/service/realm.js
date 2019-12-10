02b139c8b7924184039e2d7b8836f490c56d0891
Lucas Cunha <lucas.cunha@GRUPOIV2.LOCAL>
2019-12-10 17:18:44 -0300

    remocao dos servicoes do realm, substituidos por asyncstorage

--- src/service/realm.js
+++
@@ -1,8 +0,0 @@
-// import Realm from 'realm';
-// import NotesSchemas from '../schemas/NotesSchemas';
-
-// export default function getRealm() {
-//   return Realm.open({
-//     schema: [NotesSchemas], //aqui pode ser chamado N schemas
-//   });
-// }
