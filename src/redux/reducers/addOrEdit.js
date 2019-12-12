cd2492d2a8bfb69ddedfc901754c453156d3268e
Lucas Cunha <lucas.cunha@GRUPOIV2.LOCAL>
2019-12-12 08:46:48 -0300

    ajustes retorno funcao reducer de edicao de nota

--- src/redux/reducers/addOrEdit.js
+++ src/redux/reducers/addOrEdit.js
@@ -1,10 +1,11 @@
 const INITIAL_STATE = {
+  id: 0,
   operation: 'ADICIONAR',
 };
-export default function user(state = INITIAL_STATE, action) {
+export default function addOrEdit(state = INITIAL_STATE, action) {
   switch (action.type) {
     case 'ALTER_ADD_OR_EDIT':
-      return {...state, operation: action.textButton};
+      return {...state, id: action.id, operation: action.textButton};
     default:
       return state;
   }
